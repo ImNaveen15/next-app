d962d7f05499a5de0e6ce0d2631dbb7a188cc3a6
ImNaveen15 <42649632+ImNaveen15@users.noreply.github.com>
2024-09-13 21:35:06 +0530

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+const Customers: FC = () => {
+  return <p>Customers Page</p>;
+};
+
+export default Customers;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+const Invoices: FC = () => {
+  return <p>Invoices Page</p>;
+};
+
+export default Invoices;
